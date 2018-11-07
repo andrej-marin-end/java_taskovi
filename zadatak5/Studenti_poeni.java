7c185cf9e8a0c8768a562fef2628a92be471fff4
Andrej Marin <Andrej.Marin@endava.com>
2018-11-07 13:30:03 +0100

    Zadatak 5

---
+++ zadatak5/Studenti_poeni.java
@@ -0,0 +1,27 @@
+package zadatak5;
+import java.util.Scanner;
+
+public class Studenti_poeni {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Unesi broj studenata");
+		int brojStud = scanner.nextInt();
+		
+		System.out.println("Unesi imena studenata. Zatim i nji");
+		String imeStud;
+		int poeniStudenata = brojStud;
+		int brojPoena;
+		
+		
+		
+        for () {
+        	imeStud = scanner.next(a);
+        	projPoena = scanner.nexInt(a);
+        }
+		
+		
+		
+		
+	}
+}
