930da86d43308a5311c0311adae01386a3f72bca
Andrej Marin <Andrej.Marin@endava.com>
2018-11-15 12:16:52 +0100

    Zadatak 4

--- zadatak4/Ispis_odAdoB.java
+++ zadatak4/Ispis_odAdoB.java
@@ -1,25 +1,35 @@
 package zadatak4;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ispis_odAdoB {
 	public static void main(String[] args) {
 		
 		Scanner ulaz = new Scanner(System.in);
-		
+		try {
 		System.out.println("Unesite prvi broj:");
 		int a = ulaz.nextInt();
-		System.out.println("Unesite drugi broj:");
+        System.out.println("Unesite drugi broj:");
 		int b = ulaz.nextInt();
-		ulaz.close();		
 		int c;
-		c=a;
+            if(a>=b){
+                System.out.println("Drugi broj mora biti manji.");
+            }
+		ulaz.close();
+		c = a;
 		do	 {	
 			 if (c%2 == 0) 
 				 System.out.print(c + ", ");
 			 c++;
 		}
 			 while (c<=b);
-		}
+		
+		} catch (InputMismatchException e){
+        System.out.println("Niste uneli broj.\nPokusajte ponovo.");
+        ulaz.nextLine();
+    }	
 	}
+}
+
 
 	
